4d5b5069af14360d5b10d3788dd8713f87e0dcd4
05kashyap <aryankashyapnaveen@gmail.com>
2025-05-04 18:09:48 +0530

    updated model saving

--- NAS_trainer.py
+++ NAS_trainer.py
@@ -13,6 +13,7 @@ parser.add_argument('--batch-size', type=int, default=32, help='Batch size')
 parser.add_argument('--pecs', type=int, default=3, help='Epochs for performance estimate')
 parser.add_argument('--iters', type=int, default=20, help='NAS iterations')
 parser.add_argument('--subset-fraction', type=float, default=0.2, help='Fraction of data for each NAS sample')
+parser.add_argument('save_path', type=str,default="/kaggle/working/", help='Path to save the model')
 args = parser.parse_args()
 
 BATCH_SIZE = args.batch_size
@@ -23,7 +24,7 @@ SUBSET_FRACTION = args.subset_fraction
 # Placeholder paths
 labels_file = args.labels_file  # e.g., 'Data/archive/Frame_Pair_Labels.txt'
 base_dir = args.base_dir       # e.g., 'Data/archive/'
-
+save_path = args.save_path  # e.g., 'model_saving/fire_nas_model.pth'
 train_loader, val_loader, test_loader = get_fire_dataloaders(
     labels_file=labels_file,
     base_dir=base_dir,
@@ -31,8 +32,8 @@ train_loader, val_loader, test_loader = get_fire_dataloaders(
     train_ratio=0.8,
     val_ratio=0.1
 )
-os.mkdir('model_saving', exist_ok=True)
-model_path = 'model_saving/fire_nas_model.pth'
+os.makedirs(save_path + '/model_saving', exist_ok=True)
+model_path = save_path + '/model_saving/' + 'fire_nas_model.pth'
 search_space = FireArchitectureSearchSpace()
 controller = FireArchitectureController(search_space)
 trainer = FireNASTrainer(
